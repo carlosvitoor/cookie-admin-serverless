00d9524cb66ed86fd56bb322d55534dda9cf6448
Carlos Vitor <calosvitorsmarques@gmail.com>
2025-12-26 10:03:00 -0300

    Feat: Add src folder and link Lambda to application code

--- cookie_admin_serverless/cookie_admin_serverless_stack.py
+++ cookie_admin_serverless/cookie_admin_serverless_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
     Stack,
     aws_dynamodb as dynamodb,
-    RemovalPolicy
+    aws_lambda as _lambda,
+    RemovalPolicy,
+    Duration
 )
 from constructs import Construct
 
@@ -10,10 +12,26 @@ class CookieAdminServerlessStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, environment_tag: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Tabela DynamoDB com nome dinâmico por ambiente
+        # 1. DynamoDB
         table = dynamodb.Table(self, "CookiesTable",
             table_name=f"CookiesTable-{environment_tag}",
             partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
-            removal_policy=RemovalPolicy.DESTROY # Em Prod mudaremos para RETAIN
+            removal_policy=RemovalPolicy.DESTROY
         )
+
+        # 2. Lambda Function (Apontando para a pasta src)
+        cookie_handler = _lambda.Function(self, "CookieHandler",
+            function_name=f"CookieHandler-{environment_tag}",
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="index.handler",       # Arquivo index.py, funcao handler
+            code=_lambda.Code.from_asset("src"), # Pega o conteudo da pasta src
+            environment={
+                "TABLE_NAME": table.table_name,
+                "ENV_TYPE": environment_tag
+            },
+            timeout=Duration.seconds(10)
+        )
+
+        # 3. Permissões
+        table.grant_read_write_data(cookie_handler)
